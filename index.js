6bde762660ad41dd6552b55718793b84f44eadca
Legion <pawel.kapusta@student.uj.edu.pl>
2021-04-19 15:48:09 +0200

    fix bugs

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const cors = require('cors');
 const app = express();
 const fs = require('fs'); // I'll load up node's built in file system helper library here
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
+  res.header(
+   "Access-Control-Allow-Headers",
+   "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.header("Access-Control-Allow-Credentials", true);
+if (req.method === "OPTIONS") {
+return res.sendStatus(204);
+}
+next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
